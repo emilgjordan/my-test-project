bdb960b83f09ddc7b44f0f8410a80db4eb640faa
Emil Jordan <emil.gianni.jordan@gmail.com>
2024-09-06 11:47:45 -0700

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+const axios = require('axios');
+
+axios.get('http://localhost:3001')
+  .then(response => {
+    console.log(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+});
