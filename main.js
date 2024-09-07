292d3346acc25dbda6b3869506575514a760fd14
Emil Jordan <emil.gianni.jordan@gmail.com>
2024-09-07 08:14:38 -0700

    footer

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
-const axios = require('axios');
+const axios = require("axios");
 
-axios.get('http://localhost:3001')
-  .then(response => {
+axios
+  .get("http://localhost:3001")
+  .then((response) => {
     console.log(response.data);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
-});
+  });
+
+// by Emil Jordan
